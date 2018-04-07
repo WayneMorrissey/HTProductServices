7875abcaf6d6cff4f679bc5a1af8438ea12ae99d
Wayne <38153810+Wayne@users.noreply.github.com>
2018-04-07 03:11:52 -0600

    Start of exercise 7 up to starting web page creation

---
+++ BigPrintWebApp/ExercisePages/ProductCRUD.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductCRUD.aspx.cs" Inherits="BigPrintWebApp.ExercisePages.ProductCRUD" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="page-header">
+        <h1>Product CRUD</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+
+        </div>
+    </div>
+</asp:Content>
---
+++ HTPSSystem.WMorr/BLL/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using HTPSSystem.WMorr.Data.Entities;
+using HTPSSystem.WMorr.DAL;
+#endregion
+
+namespace HTPSSystem.WMorr.BLL
+{
+    public class ProductController
+    {
+        public List<Product> Product_List()
+        {
+            using (var context = new HTPSContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        // Returns a single product instance based on the productid
+        public Product Product_Find(int productid)
+        {
+            using (var context = new HTPSContext())
+            {
+                return context.Products.Find(productid);
+            }
+        }
+    }
+}
